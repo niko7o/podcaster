62c57a272ff9d0a3793c656594b9c310e43e3000
Nikolaas Verlee <nikotomad@gmail.com>
2023-01-16 18:04:17 +0100

    feat: created Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from '@styles/Header.module.scss';
+
+const Header = () => (
+  <div className={styles.wrapper}>
+    <h1 className={styles.title}>Podcaster</h1>
+  </div>
+)
+
+export default Header;
