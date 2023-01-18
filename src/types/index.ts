bad1ec5a49f4973dfce30ab63eb9649436ba1415
Nikolaas Verlee <nikotomad@gmail.com>
2023-01-18 16:09:21 +0100

    feat: added new id & slug fields to IPodcast type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,15 @@
 export interface IPodcast {
+  id: string,
+  slug: string,
   image: string,
   title: string,
   author: string
 }
 
 export type ApiPodcast = {
+  'id': {
+    label: string
+  },
   'im:image': ApiPodcastImage[],
   'title': {
     label: string
